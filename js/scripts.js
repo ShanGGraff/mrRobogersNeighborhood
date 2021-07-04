6e5c29be39b9d95bb8bb061afb7784bf5f70df7d
Shane Graff <copellius@gmail.com>
2021-07-04 13:58:11 -0700

    WIP: Work on intial for statement to return string Beep! for 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 //Business Logic
 function beepBoop(number) {
-  const numberInput = number;
+  const numberInput = number.parseInt.val();
   let initialArray = [];
   for (let i = 0; i <= numberInput; i++) {
-    initialArray.push(i);
+    if (numberInput = 1) {
+      initialArray.push("Beep!")
+    }
+    else {
+      initialArray.push(i);
+    }
+    
   }
   return initialArray;
 }
