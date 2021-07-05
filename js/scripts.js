5538a483ac451786bd6fec342570fa96cb75d529
Shane Graff <copellius@gmail.com>
2021-07-05 11:03:26 -0700

    Add logic for Neibhor string swap and const for output of beepBoop

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,19 @@ function createNumberArray(number) {
 function beepBoop(initialArray) {
   let beMyNeighborArray = [];
   for (let i = 0; i < initialArray.length; i++) {
-    if (initialArray[i].toString().includes("1")) {
-      beMyNeighborArray.push("Beep!");
-    }  else {
+    if (initialArray[i].toString().includes("3")) {
+      beMyNeighborArray.push("Won't you be my neighbor?")
+    } else if (initialArray[i].toString().includes("2")) {
       beMyNeighborArray.push("Boop!");
+    } else if (initialArray[i].toString().includes("1")) {
+      beMyNeighborArray.push("Beep!");
+    } else {
+      beMyNeighborArray.push(initialArray[i]);
     }
-  return beMyNeighborArray;
   }
+  return beMyNeighborArray;
 }
+
 
 //UserInterface Logic
 $(document).ready(function() {
@@ -29,7 +34,10 @@ $(document).ready(function() {
 
     const numberRange = createNumberArray($('#mrRoboger').val());
     console.log(numberRange);
-    console.log(beepBoop(numberRange));
+    
+    const stringSwap = beepBoop(numberRange);
+    console.log(beepBoop(stringSwap));
+    
   
 
     $('#mrRoboger').show(); 
